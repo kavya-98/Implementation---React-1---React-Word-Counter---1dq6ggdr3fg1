16d4481abb6a3c10fc2f0bc1b1a3022fa1118cca
kavya-98 <76950344+kavya-98@users.noreply.github.com>
2022-06-01 20:35:42 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
 const App = () => {
-
+  const [text, settext] = useState('');
 
   return (
     <div id="main">
+      <textarea onChange={(e) => settext(e.target.value)}></textarea>
+      <div className="word-counter">{text.split(' ').length - 1}</div>
+      <div id="char-counter">{text.length}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
